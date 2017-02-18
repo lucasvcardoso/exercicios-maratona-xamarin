7ac5f219ba3cb0776c2f5b306e2feabd411631ae
Lucas Vieira Cardoso <lucas.v.cardoso@gmail.com>
2017-02-18 03:22:43 -0200

    Finished writing the sample for TableLayout in the Layouts.Droid project

--- Capitulo 2 - Android/Layouts.Droid/Layouts.Droid/Resources/Resource.Designer.cs
+++ Capitulo 2 - Android/Layouts.Droid/Layouts.Droid/Resources/Resource.Designer.cs
@@ -57,6 +57,49 @@ namespace Layouts.Droid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int button2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int button3 = 2131034115;
+			
+			// aapt resource value: 0x7f050009
+			public const int editText1 = 2131034121;
+			
+			// aapt resource value: 0x7f050000
+			public const int relativeLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050005
+			public const int tableLayout1 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int tableRow1 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int tableRow2 = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView1 = 2131034119;
+			
+			// aapt resource value: 0x7f050004
+			public const int textview1 = 2131034116;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
